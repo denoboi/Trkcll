a9036a6038703338e07bc9b1e8a0fc9ef0eb94f5
denoboi <deniz.kaymakci@hotmail.com>
2021-09-17 14:19:34 +0300

    Keeb Input

---
+++ Assets/Scripts/SpaceShipController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipController : MonoBehaviour
+{
+    const float moveSpeed = 10;
+   
+    void Update()
+    {
+        float horizontalControl = Input.GetAxis("Horizontal");
+        float verticalControl = Input.GetAxis("Vertical");
+
+        Vector3 position = transform.position;
+
+        if(horizontalControl != 0)
+        {
+           position.x += horizontalControl * moveSpeed * Time.deltaTime;
+        }
+
+        transform.position = position;
+
+        if(verticalControl != 0)
+        {
+            position.y += verticalControl * moveSpeed * Time.deltaTime;
+        }
+
+        transform.position = position;
+    }
+
+}
