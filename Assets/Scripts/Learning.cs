c7c7ad28d584db59b1cf27af6855e5c05130abe5
denoboi <deniz.kaymakci@hotmail.com>
2021-09-21 18:22:22 +0300

    While Dongusu

---
+++ Assets/Scripts/Learning.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Learning : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int attackingEnemy = 10;
+        bool attackContinues = false;
+
+        //while (attackContinues)
+        //{
+        //    attackingEnemy--;
+        //    if (attackingEnemy < 3)
+        //    {
+        //        attackContinues = false;
+        //    }
+        //    Debug.Log("saldiran dusman sayisi: " + attackingEnemy);
+        //}
+
+        do
+        {
+            attackingEnemy--;
+            if (attackingEnemy < 3)
+            {
+                attackContinues = false;
+            }
+            Debug.Log("saldiran dusman sayisi: " + attackingEnemy);
+        }
+        while (attackContinues);
+       
+    }
+
+       
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
