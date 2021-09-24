a51b7333e9b646a66ad6342289f7adc07afcda75
denoboi <deniz.kaymakci@hotmail.com>
2021-09-24 20:26:34 +0300

    UI 1

---
+++ Assets/Scripts/Game/UI Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    GameObject gameNameText = default;
+    GameObject gameOverText = default;
+
+    [SerializeField]
+    Text pointsText = default;
+
+    void Start()
+    {
+        gameOverText.gameObject.SetActive(false);
+        pointsText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
